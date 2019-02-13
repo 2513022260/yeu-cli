5d756e7bc4000a1d9ce4c47ee37aed4179780a27
2513022260 <2513022260@qq.com>
2019-02-13 13:36:51 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <eu-switch v-model="lightSwitch">开关：</eu-switch>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      lightSwitch: false,
+      color: 'red'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/packages/switch/index.js
@@ -0,0 +1,5 @@
+import EuSwitch from './src/switch.vue'
+
+EuSwitch.install = Vue => Vue.component(EuSwitch.name, EuSwitch)
+
+export default EuSwitch;
